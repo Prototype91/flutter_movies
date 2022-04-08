1899b5430c4b9e756edeb2d1bff2838e296e1b3d
Prototype91 <lecomtedylan@rocketmail.com>
2022-04-08 18:04:57 +0200

    register added with firebase

---
+++ lib/screens/authentication/register.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_movies/components/form.dart';
+
+class RegisterScreen extends StatelessWidget {
+  const RegisterScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: SafeArea(
+        child: RegisterForm(),
+      ),
+    );
+  }
+}
